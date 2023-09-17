3e0bea26b73acca3c2c6cd9387623d875bedda5e
JamPo <jbpow18@icloud.com>
2023-09-17 10:20:38 -0400

    Removed verifyJWT from userRoutes.js and added to server.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const usersController = require('../controllers/usersController')
-const verifyJWT = require('../middleware/verifyJWT');
 
 router.route('/')
-    .get(verifyJWT, usersController.getAllUsers) //read
+    .get(usersController.getAllUsers) //read
     .post(usersController.createNewUser) //create
     .patch(usersController.updateUser) //update
     .delete(usersController.deleteUser) //delete
