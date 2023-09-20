7504f7f0d74506341f6cba72839ab47d0778dcc8
JamPo <jbpow18@icloud.com>
2023-09-20 04:16:52 -0400

    Code to handle user logging out

---
+++ backend/controllers/logoutController.js
@@ -0,0 +1,38 @@
+const usersDB = {
+  users: require('../models/User'),
+  setUsers: function (data) { this.users = data }
+}
+
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const handleLogout = async (request, res) => {
+
+  // On client, also delete the accessToken
+
+  const cookies = request.cookies;
+  if (!cookies?.jwt) return res.sendStatus(204); // No content to send back
+  const refreshToken = cookies.jwt;
+
+  // Is refreshToken in db?
+  const foundUser = await usersDB.users.findOne({ "refreshToken": refreshToken });
+
+  if (!foundUser) {
+    res.clearCookie('jwt', { httpOnly: true });
+    return res.sendStatus(204);
+  }
+
+  // Delete the refreshToken in db
+  const otherUsers = usersDB.users.filter(person => person.refreshToken !== foundUser);
+  const currentUser = { ...foundUser, refreshToken: '' };
+  usersDB.setUsers([...otherUsers, currentUser]);
+  await fsPromises.writeFile(
+    path.join(__dirname, '..', 'model', 'users.json'),
+    JSON.stringify(usersDB.users)
+  );
+  
+  res.clearCookie('jwt', { httpOnly: true }); // In production - would add secure: true - only serves on https
+  res.sendStatus(204);
+}
+
+module.exports = { handleLogout }
