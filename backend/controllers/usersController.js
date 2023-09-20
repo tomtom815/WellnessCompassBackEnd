d0ed4e1eb8d5a76dfac00de3adddc33f97d13580
JamPo <jbpow18@icloud.com>
2023-09-20 04:50:05 -0400

    Commented out createNewUser for now

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -31,36 +31,43 @@ const getOneUser = asyncHandler(async(req, res) => {
     res.json(user);
 });
 
+/* 
+
+Moved into registerController
+This allows a new user to be created before verifyJWT is called, 
+which requires an access token.
+
+*/
 // @desc Create new user
 // @route POST /users
 // @access Private
 
-const createNewUser = asyncHandler(async(req, res) =>{
-    const { firstName, lastName, username, password } = req.body
+// const createNewUser = asyncHandler(async(req, res) =>{
+//     const { firstName, lastName, username, password } = req.body
    
-    // confirm data
-    if(!username || !password || !firstName){
-        return res.status(400).json({ message: 'Required fields missing' })
-    }
+//     // confirm data
+//     if(!username || !password || !firstName){
+//         return res.status(400).json({ message: 'Required fields missing' })
+//     }
 
-    // check duplicates
-    const duplicate = await User.findOne({username}).lean().exec()
-    if(duplicate){
-        return res.status(409).json({message: 'Duplicate username'})
-    }
+//     // check duplicates
+//     const duplicate = await User.findOne({username}).lean().exec()
+//     if(duplicate){
+//         return res.status(409).json({message: 'Duplicate username'})
+//     }
 
-    // Hash password
-    const hashedPwd = await bcrypt.hash(password, 10) // salt rounds
-    const userObject = { username, "password": hashedPwd, firstName, lastName};
+//     // Hash password
+//     const hashedPwd = await bcrypt.hash(password, 10) // salt rounds
+//     const userObject = { username, "password": hashedPwd, firstName, lastName};
 
-    // Create and store user
-    const user = await User.create(userObject);
-    if(user){
-        res.status(201).json({message: `New user ${username} created`})
-    }else{
-        res.status(400).json({message: 'Invalid user data received'})
-    }
-});
+//     // Create and store user
+//     const user = await User.create(userObject);
+//     if(user){
+//         res.status(201).json({message: `New user ${username} created`})
+//     }else{
+//         res.status(400).json({message: 'Invalid user data received'})
+//     }
+// });
 
 // @desc update user
 // @route PATCH /users
